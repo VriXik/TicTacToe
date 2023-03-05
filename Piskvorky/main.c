43f55bb9aae161c03b1c46b6c4b632aaa265a2f4
VriXik <95382372+VriXik@users.noreply.github.com>
2023-03-05 18:29:01 +0600

    Add files via upload

--- Piskvorky/main.c
+++ Piskvorky/main.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 
 
 
 void gameBoard(char c[3][3]);
-
+char inputPlayer1(char c[3][3]);
+char inputPlayer2(char c[3][3]);
+bool check(char c[3][3]);
 
 int main() {
 
 
     char c[3][3];
-
     c[0][0]='1';
     c[0][1]='2';
     c[0][2]='3';
@@ -23,14 +25,21 @@ int main() {
 
     gameBoard(c);
 
+    do {
+
+        inputPlayer1(c);
+        gameBoard(c);
+        check(c);
+        inputPlayer2(c);
+        gameBoard(c);
+        check(c);
+
+    } while (check(c) == 1);
+
 
 
     return 0;
 }
-
-
-
-
 
 //---------------FUNKCE---------------
 
@@ -52,7 +61,7 @@ void gameBoard(char c[3][3]){
     printf("\n");
     printf("|");
     for (int j = 0; j <= 2; ++j) {
-        printf("   %d  ", c[0][j]);
+        printf("   %c  ", c[0][j]);
 
         printf("|");
     }
@@ -94,7 +103,7 @@ void gameBoard(char c[3][3]){
     printf("|");
     for (int j = 0; j <= 2; ++j) {
 
-        printf("   %d  ", c[1][j]);
+        printf("   %c  ", c[1][j]);
 
         printf("|");
     }
@@ -126,7 +135,7 @@ void gameBoard(char c[3][3]){
     printf("|");
     for (int j = 0; j <= 2; ++j) {
 
-        printf("   %d  ", c[2][j]);
+        printf("   %c  ", c[2][j]);
 
         printf("|");
     }
@@ -137,5 +146,179 @@ void gameBoard(char c[3][3]){
         printf("|");
         printf("      ");
     }
+
+}
+
+char inputPlayer1(char c[3][3]){
+
+    int input1;
+
+    printf("\nHraci 1 zadej policko\n");
+    badInput:
+    scanf("%d", &input1);
+
+    switch (input1) {
+
+        case 1:
+
+            c[0][0]='O';
+            break;
+
+        case 2:
+
+            c[0][1]='O';
+            break;
+
+        case 3:
+
+            c[0][2]='O';
+            break;
+
+        case 4:
+
+            c[1][0]='O';
+            break;
+
+        case 5:
+
+            c[1][1]='O';
+            break;
+
+        case 6:
+
+            c[1][2]='O';
+            break;
+
+        case 7:
+
+            c[2][0]='O';
+            break;
+
+        case 8:
+
+            c[2][1]='O';
+            break;
+
+        case 9:
+
+            c[2][2]='O';
+            break;
+
+        default:
+            printf("spatna hodnota zadej znovu\n");
+            goto badInput;
+    }
+}
+
+char inputPlayer2(char c[3][3]){
+
+    int input1;
+
+    printf("\nHraci 2 zadej policko\n");
+    badInput:
+    scanf("%d", &input1);
+
+    switch (input1) {
+
+        case 1:
+
+            c[0][0]='X';
+            break;
+
+        case 2:
+
+            c[0][1]='X';
+            break;
+
+        case 3:
+
+            c[0][2]='X';
+            break;
+
+        case 4:
+
+            c[1][0]='X';
+            break;
+
+        case 5:
+
+            c[1][1]='X';
+            break;
+
+        case 6:
+
+            c[1][2]='X';
+            break;
+
+        case 7:
+
+            c[2][0]='X';
+            break;
+
+        case 8:
+
+            c[2][1]='X';
+            break;
+
+        case 9:
+
+            c[2][2]='X';
+            break;
+
+        default:
+            printf("spatna hodnota zadej znovu\n");
+            goto badInput;
+    }
+}
+
+bool check(char c[3][3]){
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>ŘADEK<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+    if(c[0][0] && c[0][1] && c[0][2] == 'O' || c[0][0] && c[0][1] && c[0][2] == 'X'){
+
+
+        return 0;
+
+    }
+
+    if(c[1][0] && c[1][1] && c[1][2] == 'O' || c[1][0] && c[1][1] && c[1][2] == 'X'){
+
+
+        return 0;
+
+    }
+
+    if(c[2][0] && c[2][1] && c[2][2] == 'O' || c[2][0] && c[2][1] && c[2][2] == 'X'){
+
+
+        return 0;
+
+    }
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>SLOUPEC<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+    if(c[0][0] && c[1][0] && c[2][0] == 'O' || c[0][0] && c[1][0] && c[2][0] == 'X'){
+
+
+        return 0;
+
+    }
+
+    if(c[0][1] && c[1][1] && c[2][1] == 'O' || c[0][1] && c[1][1] && c[2][1] == 'X'){
+
+
+        return 0;
+
+    }
+
+    if(c[0][2] && c[1][2] && c[2][2] == 'O' || c[0][2] && c[1][2] && c[2][2] == 'X'){
+
+
+        return 0;
+
+    }
+
+    return 1;
 
 }
